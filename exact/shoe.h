40421411ed997dec349153fc24d28f4301a5fb1a
rmfulton <rmfulton@gmail.com>
2024-12-03 19:08:16 -0800

    defined the shoe class

--- exact/shoe.h
+++ exact/shoe.h
@@ -2,7 +2,19 @@
 #ifndef SHOE
 #define SHOE
 
-int add(int a, int b);
-int subtract(int a, int b);
+#include "Rational.h"
+class Shoe {
+    private:
+        static const unsigned num_ranks = 10;
+        int cards[num_ranks + 1];
+        int numCards;
+    public:
+        static const unsigned num_ranks = 10;
+        Rational deal_probability(int i); // should this be card ?
+        Shoe shoe_after_deal(int i);
+        bool operator==(Rational const &other);
+        bool operator!=(Rational const &other);
+        std::string toString(); // used for hashing, maybe
+};
 
 #endif
